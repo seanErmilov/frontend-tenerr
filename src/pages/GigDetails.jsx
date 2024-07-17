5e641ad2c1b3cbb26418054fcbde1585e87e0046
Your Name <hyymy9177@gmail.com>
2024-07-17 22:11:35 +0300

    Add useEffect

--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -1,12 +1,23 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-import { gigService } from '../services/gig/gig.service.local'
+import { loadGig } from '../store/actions/gig.actions'
 
 export function GigDetails() {
+
+  const { gigId } = useParams()
+  const gig = useSelector(storeState => storeState.gigModule.gig)
+  console.log(gig)
+
+  useEffect(() => {
+    loadGig(gigId)
+  }, [gigId])
 
   return (
     <section className="gig-details">
       <h2>About this gig</h2>
-      {/* <span>{title}</span> */}
+      <span>{gig}</span>
 
       <p>Hello!</p>
 
