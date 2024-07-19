353b03a407895889b33856cb0adabd0d53a51c01
Your Name <hyymy9177@gmail.com>
2024-07-19 18:32:07 +0300

    change about user  gig basic

--- src/cmps/AbuotUserGig.jsx
+++ src/cmps/AbuotUserGig.jsx
@@ -5,16 +5,12 @@ export function AboutUserGig({ user }) {
         <div className="about-user-gig">
             <h2>About User Gig</h2>
             <div className="about-user-img">
-                <img src={user.imgUrl || 'default-image-url.jpg'} alt={user.fullname || 'User'} />
+                <img src={user.imgUrl } />
             </div>
             <div className="about-user-info">
-                <div className="about-user-name">{user.fullname || 'Name not available'}</div>
-                <div className="about-user-location">From {user.location || 'Location not specified'}</div>
-                <div className="about-user-member">Member since {user.memberSince || 'Date not specified'}</div>
-                <div className="about-user-response">Avg. response time: {user.avgResponseTime || 'Not available'}</div>
-                <div className="about-user-last-delivery">Last delivery: {user.lastDelivery || 'Not available'}</div>
-                <div className="about-user-languages">Languages: {languages}</div>
-                <div className="about-user-description">{user.description || 'Description not available'}</div>
+                <div className="about-user-name">{user.fullname }</div>
+                <div className="about-user-level">{user.level }</div>
+                <div className="about-user-rate">{'★' + user.rate}</div>
             </div>
         </div>
     );
