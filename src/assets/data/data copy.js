98e25f2438d0cfb4284c9bf2bffaff459e28e200
seanErmilov <seanermilov321@gmail.com>
2024-07-25 17:10:46 +0300

    add jason

---
+++ src/assets/data/data copy.js
@@ -0,0 +1,209 @@
+[
+    {
+        "title": "I will create a website for you",
+        "price": 67,
+        "owner": {
+            "_id": "u102",
+            "fullname": "Jane Doe",
+            "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU7_D7oHLUDST-89GtjX2R4i3OPZSkCEICQw&s",
+            "level": 3,
+            "diamonds": 3,
+            "rate": 5,
+            "description": "Specializing in building responsive and user-friendly websites."
+        },
+        "daysToMake": 12,
+        "description": "Professional Logo Clean, modern and custom logo with high-quality standards! Im a full time graphic designer offering creative logo design service. My gig is different from other logo design gigs since Im offering unlimited concepts and unlimited revisions for the most competitive price. Designing new concepts is something very interesting to me. Im a senior creative graphic designer, with expertise in illustration and animation - all the skills needed for bespoke and original logos UNLIMITED original and unique concepts until you are 100% happy UNLIMITED revisions and color options based on your requirements and branding PROFESSIONAL customer service and communication at all times, replies are instant! UNRUSHED service where quality comes first GUARANTEED fast delivery PROFESSIONAL team of 3 full time graphic designers LIFETIME support on ALL logo designs.",
+        "avgResponseTime": 12,
+        "loc": "Brazil",
+        "imgUrls": [
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/v1/attachments/delivery/asset/27cf03bd71a2545f9671f1e9d32b5980-1720730662/SAVE_20240711_224328/build-your-wordpress-website-with-oxygen-builder.jpg",
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/v1/attachments/delivery/asset/18f0ed6d24c12557a40244aadbe6c572-1720003890/Lost_love_final/design-unique-cover-art.jpg",
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/v1/attachments/delivery/asset/d6234c03522f05c4e835918a663db0b4-1689160324/mooifutnou%20nl-A1%20Live%20Moc/design-minimalist-logo-design.jpg",
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/v1/attachments/delivery/asset/870c4e3c181ccd1361fa99aab58f71d8-1689426735/Cosmic%20Highs%20Logo_A%20%20Live%20Moc/design-minimalist-logo-design.jpg"
+        ],
+        "tags": ["lifestyle", "business"],
+        "likedByUsers": ["mini-user"],
+        "reviews": [
+            {
+                "id": "r74861",
+                "txt": "Stefan is a pleasure to work with. Well consider using him again for future projects! He took our directions and presented a report that will be used for the coming years to communicate our plans effectively.",
+                "rate": 5,
+                "by": {
+                    "_id": "a101",
+                    "fullname": "John Smith",
+                    "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaXrFMnQrS3cdGFTB-UpG-5qMGMQyybPu7xg&s",
+                    "level": 1,
+                    "diamonds": 1,
+                    "rate": 3,
+                    "description": "A skilled writer with a knack for crafting engaging and informative articles."
+                }
+            },
+            {
+                "id": "r88537",
+                "txt": "Creative and unique approach to design. Loved it!",
+                "rate": 5,
+                "by": {
+                    "_id": "u102",
+                    "fullname": "Jane Doe",
+                    "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU7_D7oHLUDST-89GtjX2R4i3OPZSkCEICQw&s",
+                    "level": 3,
+                    "diamonds": 3,
+                    "rate": 5,
+                    "description": "Specializing in building responsive and user-friendly websites."
+                }
+            },
+            {
+                "id": "r29568",
+                "txt": "Excellent service",
+                "rate": 5,
+                "by": {
+                    "_id": "u101",
+                    "fullname": "Dudu Da",
+                    "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3afO247RTO80vqElpg1Iyapzzg-d-bfeRxQ&s",
+                    "level": 2,
+                    "diamonds": 2,
+                    "rate": 4,
+                    "description": "An experienced logo designer with a passion for creating unique."
+                }
+            }
+        ]
+    },
+    {
+        "title": "I will design your logo",
+        "price": 53,
+        "owner": {
+            "_id": "a101",
+            "fullname": "John Smith",
+            "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaXrFMnQrS3cdGFTB-UpG-5qMGMQyybPu7xg&s",
+            "level": 1,
+            "diamonds": 1,
+            "rate": 3,
+            "description": "A skilled writer with a knack for crafting engaging and informative articles."
+        },
+        "daysToMake": 5,
+        "description": "Professional Logo Clean, modern and custom logo with high-quality standards! Im a full time graphic designer offering creative logo design service. My gig is different from other logo design gigs since Im offering unlimited concepts and unlimited revisions for the most competitive price. Designing new concepts is something very interesting to me. Im a senior creative graphic designer, with expertise in illustration and animation - all the skills needed for bespoke and original logos UNLIMITED original and unique concepts until you are 100% happy UNLIMITED revisions and color options based on your requirements and branding PROFESSIONAL customer service and communication at all times, replies are instant! UNRUSHED service where quality comes first GUARANTEED fast delivery PROFESSIONAL team of 3 full time graphic designers LIFETIME support on ALL logo designs.",
+        "avgResponseTime": 9,
+        "loc": "India",
+        "imgUrls": [
+            "https://fiverr-res.cloudinary.com/images/t_smartwm/t_main1,q_auto,f_auto,q_auto,f_auto/v1/attachments/delivery/asset/c138cfdf4859bb497ff904beeb4be5f8-1717583961/Creative_self_new/design-unique-cover-art.jpg",
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs3/23197467/original/c16ce5a0377572be80eee0bfda3c95c6361b04bd/design-an-impressive-logo.jpg",
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/23197467/original/d4f9121d0d962301e3ef52c1a5217bf003889e2a/design-an-impressive-logo.jpg",
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/v1/attachments/delivery/asset/6d0e10f04e7584ad639b37cbca8d5055-1720537917/SAVE_20240709_163640/build-your-wordpress-website-with-oxygen-builder.jpg"
+        ],
+        "tags": ["ai", "music"],
+        "likedByUsers": ["mini-user"],
+        "reviews": [
+            {
+                "id": "r42258",
+                "txt": "Superb communication and outstanding results!",
+                "rate": 4,
+                "by": {
+                    "_id": "u101",
+                    "fullname": "Dudu Da",
+                    "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3afO247RTO80vqElpg1Iyapzzg-d-bfeRxQ&s",
+                    "level": 2,
+                    "diamonds": 2,
+                    "rate": 4,
+                    "description": "An experienced logo designer with a passion for creating unique."
+                }
+            },
+            {
+                "id": "r83236",
+                "txt": "Stefan is a pleasure to work with. Well consider using him again for future projects! He took our directions and presented a report that will be used for the coming years to communicate our plans effectively.",
+                "rate": 5,
+                "by": {
+                    "_id": "a101",
+                    "fullname": "John Smith",
+                    "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaXrFMnQrS3cdGFTB-UpG-5qMGMQyybPu7xg&s",
+                    "level": 1,
+                    "diamonds": 1,
+                    "rate": 3,
+                    "description": "A skilled writer with a knack for crafting engaging and informative articles."
+                }
+            },
+            {
+                "id": "r47674",
+                "txt": "Excellent service",
+                "rate": 4,
+                "by": {
+                    "_id": "u101",
+                    "fullname": "Dudu Da",
+                    "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3afO247RTO80vqElpg1Iyapzzg-d-bfeRxQ&s",
+                    "level": 2,
+                    "diamonds": 2,
+                    "rate": 4,
+                    "description": "An experienced logo designer with a passion for creating unique."
+                }
+            }
+        ]
+    },
+    {
+        "title": "I will write an article for you",
+        "price": 82,
+        "owner": {
+            "_id": "a101",
+            "fullname": "John Smith",
+            "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaXrFMnQrS3cdGFTB-UpG-5qMGMQyybPu7xg&s",
+            "level": 1,
+            "diamonds": 1,
+            "rate": 3,
+            "description": "A skilled writer with a knack for crafting engaging and informative articles."
+        },
+        "daysToMake": 6,
+        "description": "Professional Logo Clean, modern and custom logo with high-quality standards! Im a full time graphic designer offering creative logo design service. My gig is different from other logo design gigs since Im offering unlimited concepts and unlimited revisions for the most competitive price. Designing new concepts is something very interesting to me. Im a senior creative graphic designer, with expertise in illustration and animation - all the skills needed for bespoke and original logos UNLIMITED original and unique concepts until you are 100% happy UNLIMITED revisions and color options based on your requirements and branding PROFESSIONAL customer service and communication at all times, replies are instant! UNRUSHED service where quality comes first GUARANTEED fast delivery PROFESSIONAL team of 3 full time graphic designers LIFETIME support on ALL logo designs.",
+        "avgResponseTime": 15,
+        "loc": "USA",
+        "imgUrls": [
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs3/263804107/original/ae4e4fc77fa5100fe035d9d167b1cc28e0d78e0b/build-your-wordpress-website-with-oxygen-builder.png",
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs2/115722335/original/5655a351acead96fa69c29acde4abe0948d585f6/design-minimalist-logo-design.jpg",
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/v1/attachments/delivery/asset/27cf03bd71a2545f9671f1e9d32b5980-1720730662/SAVE_20240711_224328/build-your-wordpress-website-with-oxygen-builder.jpg",
+            "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/v1/attachments/delivery/asset/55f9c7f8c06734f6aa2cae52e05c8f63-1690531981/CASA%202%20%20logo-E5%20Live%20Moc/design-minimalist-logo-design.jpg"
+        ],
+        "tags": ["graphics", "lifestyle"],
+        "likedByUsers": ["mini-user"],
+        "reviews": [
+            {
+                "id": "r78434",
+                "txt": "Creative and unique approach to design. Loved it!",
+                "rate": 5,
+                "by": {
+                    "_id": "u101",
+                    "fullname": "Dudu Da",
+                    "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3afO247RTO80vqElpg1Iyapzzg-d-bfeRxQ&s",
+                    "level": 2,
+                    "diamonds": 2,
+                    "rate": 4,
+                    "description": "An experienced logo designer with a passion for creating unique."
+                }
+            },
+            {
+                "id": "r99434",
+                "txt": "Stefan is a pleasure to work with. Well consider using him again for future projects! He took our directions and presented a report that will be used for the coming years to communicate our plans effectively.",
+                "rate": 5,
+                "by": {
+                    "_id": "a101",
+                    "fullname": "John Smith",
+                    "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaXrFMnQrS3cdGFTB-UpG-5qMGMQyybPu7xg&s",
+                    "level": 1,
+                    "diamonds": 1,
+                    "rate": 3,
+                    "description": "A skilled writer with a knack for crafting engaging and informative articles."
+                }
+            },
+            {
+                "id": "r26358",
+                "txt": "Superb communication and outstanding results!",
+                "rate": 4,
+                "by": {
+                    "_id": "u101",
+                    "fullname": "Dudu Da",
+                    "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3afO247RTO80vqElpg1Iyapzzg-d-bfeRxQ&s",
+                    "level": 2,
+                    "diamonds": 2,
+                    "rate": 4,
+                    "description": "An experienced logo designer with a passion for creating unique."
+                }
+            }
+        ]
+    }
+]
