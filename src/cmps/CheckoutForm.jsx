84f9e084460a0f04fd3f9ebd7625ba023eb4ac20
Your Name <hyymy9177@gmail.com>
2024-07-28 17:02:22 +0300

    Scss chechout

--- src/cmps/CheckoutForm.jsx
+++ src/cmps/CheckoutForm.jsx
@@ -50,60 +50,70 @@ export function CheckoutForm({ order }) {
       >
         {({ errors, touched, values, handleChange, setFieldValue }) => (
           <Form>
+            <div className='item'>
+              <h2>Card Number</h2>
+              <Field
+                as={TextField}
+                // label="CardNumber"
+                variant="outlined"
+                type="number"
+                name="cardnumber"
+                margin="normal"
+                inputProps={{ min: 1 }}
+                error={touched.cardnumber && !!errors.cardnumber}
+                helperText={touched.cardnumber && errors.cardnumber}
+                onChange={handleChange}
+                value={values.cardnumber}
+              />
+            </div>
+            <div className='form-center'>
+            <div className='item'>                <h2>Expiration Date</h2>
+                <Field
+                  as={TextField}
+                  // label="ExpirationDate"
+                  variant="outlined"
+                  type="number"
+                  name="expirationdate"
+                  margin="normal"
+                  inputProps={{ min: 1 }}
+                  error={touched.expirationdate && !!errors.expirationdate}
+                  helperText={touched.expirationdate && errors.expirationdate}
+                  onChange={handleChange}
+                  value={values.expirationdate}
+                />
+              </div>
 
-            <Field
-              as={TextField}
-              label="CardNumber"
-              variant="outlined"
-              type="number"
-              name="cardnumber"
-              margin="normal"
-              inputProps={{ min: 1 }}
-              error={touched.cardnumber && !!errors.cardnumber}
-              helperText={touched.cardnumber && errors.cardnumber}
-              onChange={handleChange}
-              value={values.cardnumber}
-            />
 
-            <Field
-              as={TextField}
-              label="ExpirationDate"
-              variant="outlined"
-              type="number"
-              name="expirationdate"
-              margin="normal"
-              inputProps={{ min: 1 }}
-              error={touched.expirationdate && !!errors.expirationdate}
-              helperText={touched.expirationdate && errors.expirationdate}
-              onChange={handleChange}
-              value={values.expirationdate}
-            />
+              <div className='item'>                <h2>Security Code</h2>
+                <Field
+                  as={TextField}
+                  // label="SecurityCode"
+                  variant="outlined"
+                  type="number"
+                  name="securitycode"
+                  margin="normal"
+                  inputProps={{ min: 1 }}
+                  error={touched.securitycode && !!errors.securitycode}
+                  helperText={touched.securitycode && errors.securitycode}
+                  onChange={handleChange}
+                  value={values.securitycode}
+                />
+              </div>
 
-            <Field
-              as={TextField}
-              label="SecurityCode"
-              variant="outlined"
-              type="number"
-              name="securitycode"
-              margin="normal"
-              inputProps={{ min: 1 }}
-              error={touched.securitycode && !!errors.securitycode}
-              helperText={touched.securitycode && errors.securitycode}
-              onChange={handleChange}
-              value={values.securitycode}
-            />
-
-            <Field
-              as={TextField}
-              label="Cardholder's name"
-              variant="outlined"
-              name="name"
-              margin="normal"
-              error={touched.name && !!errors.name}
-              helperText={touched.name && errors.name}
-              onChange={handleChange}
-              value={values.name}
-            />
+            </div>
+            <div className='item'>              <h2>Card holder's name</h2>
+              <Field
+                as={TextField}
+                // label="Cardholder's name"
+                variant="outlined"
+                name="name"
+                margin="normal"
+                error={touched.name && !!errors.name}
+                helperText={touched.name && errors.name}
+                onChange={handleChange}
+                value={values.name}
+              />
+            </div>
 
           </Form>
         )}
