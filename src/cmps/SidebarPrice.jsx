ca683e985e324f0980e57d69438104059af6949b
amirboxer <amirboxer95@gmail.com>
2024-07-28 19:03:01 +0300

    Fix style to side price bar

--- src/cmps/SidebarPrice.jsx
+++ src/cmps/SidebarPrice.jsx
@@ -11,7 +11,7 @@ export function SidebarPrice({ handelcheckout, price, avgResponseTime, handleOpe
 
     const packages = {
         normal: {
-            name: 'Normal',
+            name: 'Basic',
             description: 'Basic package with standard features',
             price: price,
             avgResponseTime: avgResponseTime,
@@ -24,10 +24,9 @@ export function SidebarPrice({ handelcheckout, price, avgResponseTime, handleOpe
                 { name: "Include source file", included: false },
                 { name: "Stationery designs", included: false },
             ],
-            colors: ['black', 'gray', 'gray', 'gray', 'gray', 'gray', 'gray']
         },
         medium: {
-            name: 'Medium',
+            name: 'Standard',
             description: 'Enhanced package with additional features',
             price: price * 2,
             avgResponseTime: avgResponseTime + 1,
@@ -40,7 +39,6 @@ export function SidebarPrice({ handelcheckout, price, avgResponseTime, handleOpe
                 { name: "Include source file", included: false },
                 { name: "Stationery designs", included: false },
             ],
-            colors: ['black', 'black', 'gray', 'gray', 'gray', 'gray', 'gray']
         },
         premium: {
             name: 'Premium',
@@ -92,12 +90,11 @@ export function SidebarPrice({ handelcheckout, price, avgResponseTime, handleOpe
 
             <section>
                 <div className="package-details">
-                    <h4>{packages[selectedPackage].name}</h4>
+                    <h3 className='sidebar-des'>{packages[selectedPackage].description}</h3>
                     <div>
                         <label> ₪{packages[selectedPackage].price}</label>
                     </div>
                 </div>
-                <p className='sidebar-des'>{packages[selectedPackage].description}</p>
                 <h1 className='sidebar-delivery'>
                     <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNOCAwQzMuNiAwIDAgMy42IDAgOHMzLjYgOCA4IDggOC0zLjYgOC04LTMuNi04LTgtOHptMCAxNGMtMy4zIDAtNi0yLjctNi02czIuNy02IDYtNiA2IDIuNyA2IDYtMi43IDYtNiA2eiIvPjxwYXRoIGQ9Ik05IDRIN3Y1aDVWN0g5VjR6Ii8+PC9zdmc+" />{packages[selectedPackage].avgResponseTime}-day delivery
                     <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNNC41MDAwMSAxMS40OTk5QzYuNDAwMDEgMTMuMzk5OSA5LjYwMDAxIDEzLjM5OTkgMTEuNSAxMS40OTk5QzEyLjIgMTAuNzk5OSAxMi43IDkuNzk5OSAxMi45IDguNzk5OUwxNC45IDkuMDk5OUMxNC43IDEwLjU5OTkgMTQgMTEuODk5OSAxMyAxMi44OTk5QzEwLjMgMTUuNTk5OSA1LjkwMDAxIDE1LjU5OTkgMy4xMDAwMSAxMi44OTk5TDAuOTAwMDEyIDE1LjA5OTlMMC4yMDAwMTIgOC42OTk5TDYuNjAwMDEgOS4zOTk5TDQuNTAwMDEgMTEuNDk5OVoiLz48cGF0aCBkPSJNMTUuOCA3LjI5OTlMOS40MDAwMSA2LjU5OTlMMTEuNSA0LjQ5OTlDOS42MDAwMSAyLjU5OTkgNi40MDAwMSAyLjU5OTkgNC41MDAwMSA0LjQ5OTlDMy44MDAwMSA1LjE5OTkgMy4zMDAwMSA2LjE5OTkgMy4xMDAwMSA3LjE5OTlMMS4xMDAwMSA2Ljg5OTlDMS4zMDAwMSA1LjM5OTkgMi4wMDAwMSA0LjA5OTkgMy4wMDAwMSAzLjA5OTlDNC40MDAwMSAxLjY5OTkgNi4xMDAwMSAxLjA5OTkgNy45MDAwMSAxLjA5OTlDOS43MDAwMSAxLjA5OTkgMTEuNSAxLjc5OTkgMTIuOCAzLjA5OTlMMTUgMC44OTk5MDJMMTUuOCA3LjI5OTlaIi8+PC9zdmc+" />{packages[selectedPackage].avgResponseTime + 3} Revisions
@@ -110,15 +107,7 @@ export function SidebarPrice({ handelcheckout, price, avgResponseTime, handleOpe
                     {!isCollapsed && (
                         <ul className="feature-list">
                             {packages[selectedPackage].features.map((feature, index) => (
-                                <li
-                                    key={index}
-                                    style={{
-                                        color: packages[selectedPackage].colors[index],
-                                        listStyleType: 'none',
-                                        display: 'flex',
-                                        alignItems: 'center'
-                                    }}
-                                >
+                                <li>
                                     <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxMSA5IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGZpbGw9ImN1cnJlbnRGaWxsIj48cGF0aCBkPSJNMy42NDUgOC4xMDIuMTU4IDQuNjE1YS41MzYuNTM2IDAgMCAxIDAtLjc1OWwuNzU5LS43NThjLjIxLS4yMS41NDktLjIxLjc1OCAwbDIuMzUgMi4zNDlMOS4wNTQuNDE2Yy4yMS0uMjEuNTUtLjIxLjc1OSAwbC43NTguNzU4Yy4yMS4yMS4yMS41NSAwIC43NTlMNC40MDMgOC4xMDJjLS4yMDkuMjEtLjU0OS4yMS0uNzU4IDBaIi8+PC9zdmc+" />
                                     {feature.name}
                                 </li>
