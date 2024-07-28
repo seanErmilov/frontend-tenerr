147946dd7d16b1f2909f504d6ce45db5d148b60c
amirboxer <amirboxer95@gmail.com>
2024-07-28 03:08:49 +0300

    Change index margins

--- src/pages/GigIndex.jsx
+++ src/pages/GigIndex.jsx
@@ -1,9 +1,8 @@
 
-import { useEffect} from 'react'
-import { useSelector} from 'react-redux'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 import { loadGigs, setFilter } from '../store/actions/gig.actions'
-import { HEADER_STICKY } from '../store/reducers/system.reducer'
 
 import { GigList } from '../cmps/GigList'
 import { GigFilter } from '../cmps/GigFilter'
@@ -41,11 +40,8 @@ export function GigIndex() {
     const innerText = filterBy.title ? filterBy.title : filterBy.tags[0]
     return (
         <main className="gig-index">
-
             <Inside filterBy={filterBy}
                 innerText={innerText} />
-            <header>
-            </header>
             <h1>Logo Design</h1>
             <h4>Stand out from the crowd with a logo that fits your brand personality</h4>
             <FilterBtn />
