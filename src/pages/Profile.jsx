166addee2e146cf7a80bafd7142fd83086a17ed6
Your Name <hyymy9177@gmail.com>
2024-07-30 19:31:12 +0300

    ADD online in propile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,13 +2,19 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export function Profile() {
-	const user = useSelector(storeState => storeState.userModule.user)
+    const user = useSelector(storeState => storeState.userModule.user)
 
     return (
         <section className="seller-profile">
             <section className="content-wrapper">
                 <div className="profile-container">
                     <div className="user-profil">
+                  
+                                    <div className="online-status">
+                                        <span className="online-indicator"></span>
+                                        <span className="online-text">Online</span>
+                                    </div>
+                            
                         <div className="img-container">
                             <img src={user.imgUrl} />
                         </div>
@@ -18,7 +24,8 @@ export function Profile() {
                                 <li className="flex space-between">
                                     <div>
                                         <span><img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTIiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxMiAxNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBmaWxsPSIjNjI2NDZhIj48ZyBjbGlwLXBhdGg9InVybCgjY2xpcDApIj48cGF0aCBkPSJNNS4zODMzOCAxNS42NzcyQzAuODQyODEzIDkuMDk0NzIgMCA4LjQxOTE2IDAgNkMwIDIuNjg2MjggMi42ODYyOCAwIDYgMEM5LjMxMzcyIDAgMTIgMi42ODYyOCAxMiA2QzEyIDguNDE5MTYgMTEuMTU3MiA5LjA5NDcyIDYuNjE2NjIgMTUuNjc3MkM2LjMxODY2IDE2LjEwNzYgNS42ODEzMSAxNi4xMDc2IDUuMzgzMzggMTUuNjc3MlpNNiA4LjVDNy4zODA3MiA4LjUgOC41IDcuMzgwNzIgOC41IDZDOC41IDQuNjE5MjggNy4zODA3MiAzLjUgNiAzLjVDNC42MTkyOCAzLjUgMy41IDQuNjE5MjggMy41IDZDMy41IDcuMzgwNzIgNC42MTkyOCA4LjUgNiA4LjVaIi8+PC9nPjxkZWZzPjxjbGlwUGF0aCBpZD0iY2xpcDAiPjxyZWN0IHdpZHRoPSIxMiIgaGVpZ2h0PSIxNiIvPjwvY2xpcFBhdGg+PC9kZWZzPjwvc3ZnPg==" /></span>
-                                        Country</div>
+                                        Country
+                                    </div>
                                     <span>United States</span>
                                 </li>
                                 <li className="flex space-between">
@@ -28,15 +35,16 @@ export function Profile() {
                                     </div>
                                     <span>June 2024</span>
                                 </li>
-
+                             
                             </ul>
                         </div>
+
                     </div>
                     <div className="description-container">
                         <ul>
                             <li>
                                 <h3>Description</h3>
-                                <p>Hello, this is Frederick, stand up for vividstore,I am a young and enthusiastic graphic artist and realistic pencil sketch artist. I am certified as graphic designer from George Washington University, USA. I have almost 11 years experience in this field since my university life. I really love to work with Adobe Illustrator, Adobe Photoshop, and so on as a full time online freelancer. And also passionate about sketching. Thank you.</p>
+                                <p>Hello, this is {user.fullname}, stand up for vividstore,I am a young and enthusiastic graphic artist and realistic pencil sketch artist. I am certified as graphic designer from George Washington University, USA. I have almost 11 years experience in this field since my university life. I really love to work with Adobe Illustrator, Adobe Photoshop, and so on as a full time online freelancer. And also passionate about sketching. Thank you.</p>
                             </li>
                         </ul>
                     </div>
