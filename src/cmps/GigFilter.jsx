fa9056438f860fb33210b2ecfab20e70689504ff
amirboxer <amirboxer95@gmail.com>
2024-07-31 10:59:26 +0300

    Integrate Fillter into component

--- src/cmps/GigFilter.jsx
+++ src/cmps/GigFilter.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { gigService } from '../services/gig'
 import { SearchBar } from './SearchBar'
 import { useEffectUpdate } from '../customHooks/useEffectUpdate'
@@ -8,6 +8,23 @@ export function GigFilter({ filterBy, setFilterBy }) {
     const [filterToEdit, setFilterToEdit] = useState(structuredClone(filterBy))
     const [costumPrice, setCostumPrice] = useState()
 
+    const [priceOptionsVisible, setPriceListVisible] = useState(false)
+    const [dateOptionsVisible, setDateListVisible] = useState(false)
+
+
+    const priceList = useRef(null)
+    const dateList = useRef(null)
+
+
+
+    useEffect(() => {
+        if (priceList.current && priceOptionsVisible) priceList.current.focus()
+    }, [priceOptionsVisible])
+    
+    useEffect(() => {
+        if (dateList.current && dateOptionsVisible) dateList.current.focus()
+
+    }, [dateOptionsVisible])
 
     useEffectUpdate(() => {
         setFilterBy(filterToEdit)
@@ -27,23 +44,14 @@ export function GigFilter({ filterBy, setFilterBy }) {
         setCostumPrice(val)
     }
 
-    // function handleChange(ev) {
-    //     const type = ev.target.type
-    //     const field = ev.target.name
-    //     let value
+    function handleBlur(e, setter) {
+        if (e.currentTarget.contains(e.relatedTarget) || e.relatedTarget && e.relatedTarget.dataset.preventchildblur) return
 
-    //     switch (type) {
-    //         case 'text':
-    //         case 'radio':
-    //             value = field === 'sortDir' ? +ev.target.value : ev.target.value
-    //             if (!filterToEdit.sortDir) filterToEdit.sortDir = 1
-    //             break
-    //         case 'number':
-    //             value = +ev.target.value || ''
-    //             break
-    //     }
-    //     setFilterToEdit({ ...filterToEdit, [field]: value })
-    // }
+        setter(false)
+    }
+
+
+
 
     function clearFilter() {
         setFilterToEdit({ ...filterToEdit, txt: '', minSpeed: '', maxPrice: '' })
@@ -54,70 +62,73 @@ export function GigFilter({ filterBy, setFilterBy }) {
     }
 
     return <div className="filter-btn-container">
-        <div className="filter-btn"><span>Budget</span>
-            <div className='index-filter-container'>
-                <form action="" onSubmit={ev => onfilterBySubmit(ev, 'price')}>
+        <div className="filter-btn" onClick={() => setPriceListVisible(prev => !prev)} tabIndex="-1" data-preventchildblur="prices-btn"><span>Budget</span>
+            {/* price */}
+            {priceOptionsVisible &&
+                <div className='index-filter-container' ref={priceList} onBlur={e => handleBlur(e, setPriceListVisible)} tabIndex="-1" onClick={e => e.stopPropagation()}>
+                    <form action="" onSubmit={ev => onfilterBySubmit(ev, 'price')}>
 
-                    <div className='opt'>
-                        <input type="radio" id="huey" name="price" value="50" defaultChecked />
-                        <label htmlFor="huey"><span>value</span>Under $50</label>
-                    </div>
+                        <div className='opt'>
+                            <input type="radio" id="huey" name="price" value="50" defaultChecked />
+                            <label htmlFor="huey"><span>value</span>Under $50</label>
+                        </div>
 
-                    <div className='opt'>
-                        <input type="radio" id="dewey" name="price" value="100" />
-                        <label htmlFor="dewey"><span>value</span>Under $100</label>
-                    </div>
+                        <div className='opt'>
+                            <input type="radio" id="dewey" name="price" value="100" />
+                            <label htmlFor="dewey"><span>value</span>Under $100</label>
+                        </div>
 
-                    <div className='opt'>
-                        <input type="radio" id="louie" name="price" value="200" />
-                        <label htmlFor="louie"><span>value</span>Under $200</label>
-                    </div>
+                        <div className='opt'>
+                            <input type="radio" id="louie" name="price" value="200" />
+                            <label htmlFor="louie"><span>value</span>Under $200</label>
+                        </div>
 
-                    <div className='costum opt'>
-                        <input type="radio" id="toopy" name="price" value={costumPrice} />
-                        <label htmlFor="toopy">Costum
-                            <br />
-                            <input type="number" name="" id="" onChange={e => onChamgeCostumPrice(e.target.value)} placeholder='Enter Budget                     $' />
-                        </label>
-                    </div>
+                        <div className='costum opt'>
+                            <input type="radio" id="toopy" name="price" value={costumPrice} />
+                            <label htmlFor="toopy">Costum
+                                <br />
+                                <input type="number" name="" id="" onChange={e => onChamgeCostumPrice(e.target.value)} placeholder='Enter Budget                     $' />
+                            </label>
+                        </div>
 
-                    <div className='btn-container'>
-                        <button type="submit" className='apply'>Apply</button>
-                    </div>
-                </form>
-            </div>
+                        <div className='btn-container'>
+                            <button type="submit" className='apply'>Apply</button>
+                        </div>
+                    </form>
+                </div>}
         </div>
 
-        <div className="filter-btn"><span>Delivery time</span>
+        {/* dates */}
+        <div className="filter-btn" onClick={() => setDateListVisible(prev => !prev)} tabIndex="-1" data-preventchildblur="date-btn"><span>Delivery time</span>
+            {dateOptionsVisible &&
 
-            <div className='index-filter-container'>
-                <form action="" onSubmit={ev => onfilterBySubmit(ev, 'daysToMake')}>
+                <div className='index-filter-container' ref={dateList} onBlur={e => handleBlur(e, setDateListVisible)} tabIndex="-1" onClick={e => e.stopPropagation()}>
+                    <form action="" onSubmit={ev => onfilterBySubmit(ev, 'daysToMake')}>
+                        <div className='opt'>
+                            <input type="radio" id="huey" name="daysToMake" value="1" />
+                            <label htmlFor="huey"><span>Express 24H</span></label>
+                        </div>
 
-                    <div className='opt'>
-                        <input type="radio" id="huey" name="daysToMake" value="1" />
-                        <label htmlFor="huey"><span>Express 24H</span></label>
-                    </div>
+                        <div className='opt'>
+                            <input type="radio" id="dewey" name="daysToMake" value="3" />
+                            <label htmlFor="dewey"><span>Up to 3 days</span></label>
+                        </div>
 
-                    <div className='opt'>
-                        <input type="radio" id="dewey" name="daysToMake" value="3" />
-                        <label htmlFor="dewey"><span>Up to 3 days</span></label>
-                    </div>
+                        <div className='opt'>
+                            <input type="radio" id="louie" name="daysToMake" value="7" />
+                            <label htmlFor="louie"><span>Up to 7 days</span></label>
+                        </div>
 
-                    <div className='opt'>
-                        <input type="radio" id="louie" name="daysToMake" value="7" />
-                        <label htmlFor="louie"><span>Up to 7 days</span></label>
-                    </div>
+                        <div className='opt'>
+                            <input type="radio" id="toopy" name="daysToMake" value={Infinity} defaultChecked />
+                            <label htmlFor="toopy"><span>Anytime</span></label>
+                        </div>
 
-                    <div className='opt'>
-                        <input type="radio" id="toopy" name="daysToMake" value={Infinity} defaultChecked />
-                        <label htmlFor="toopy"><span>Anytime</span></label>
-                    </div>
-
-                    <div className='btn-container'>
-                        <button type="submit" className='apply'>Apply</button>
-                    </div>
-                </form>
-            </div>
+                        <div className='btn-container'>
+                            <button type="submit" className='apply'>Apply</button>
+                        </div>
+                    </form>
+                </div>}
         </div>
     </div>
 }
--- src/pages/GigIndex.jsx
+++ src/pages/GigIndex.jsx
@@ -22,31 +22,12 @@ export function GigIndex() {
     const defaultFilter = gigService.getFilterFromSearchParams(searchParams)
     const filterBy = useSelector((storeState) => storeState.gigModule.filterBy)
 
-    const [costumPrice, setCostumPrice] = useState()
-    const [priceOptionsVisible, setPriceListVisible] = useState(false)
-    const [dateOptionsVisible, setDateListVisible] = useState(false)
-
-    // console.log(dateOptionsVisible)
-
-
-    const priceList = useRef(null)
-    const dateList = useRef(null)
-
-
     const gigs = useSelector(storeState => storeState.gigModule.gigs)
 
     useEffect(() => {
         setFilter(defaultFilter)
     }, [])
 
-    useEffect(() => {
-        if (dateList.current && dateOptionsVisible) dateList.current.focus()
-
-    }, [dateOptionsVisible])
-
-    useEffect(() => {
-        if (priceList.current && priceOptionsVisible) priceList.current.focus()
-    }, [priceOptionsVisible])
 
     useEffect(() => {
         loadGigs(filterBy)
@@ -59,38 +40,6 @@ export function GigIndex() {
 
 
 
-
-    function onfilterBySubmit(ev, type) {
-        ev.preventDefault()
-
-        const formData = new FormData(ev.target)
-
-        const selectedPrice = +formData.get(type)
-
-        if (!selectedPrice || selectedPrice <= 0) return
-
-        console.log("Selected Price:", selectedPrice)
-    }
-
-
-    function onfilterByDateSubmit(ev) {
-        ev.preventDefault()
-
-        const formData = new FormData(ev.target);
-
-        const selectedDate = +formData.get('date')
-
-        if (!selectedDate || selectedDate <= 0) return
-        console.log("Selected date:", selectedDate)
-    }
-
-    function handleBlur(e, setter) {
-        if (e.currentTarget.contains(e.relatedTarget) || e.relatedTarget && e.relatedTarget.dataset.preventchildblur) return
-
-        setter(false)
-    }
-
-
     if (!gigs.length) return
     const innerText = filterBy.title ? filterBy.title : filterBy.tags[0]
     return (
@@ -100,76 +49,6 @@ export function GigIndex() {
 
             <h1>Logo Design</h1>
             <h4>Stand out from the crowd with a logo that fits your brand personality</h4>
-            <div className="filter-btn-container">
-                <div className="filter-btn" onClick={() => setPriceListVisible(prev => !prev)} tabIndex="-1" data-preventchildblur="prices-btn"><span>Budget</span>
-                    {/* price */}
-                    {priceOptionsVisible &&
-                        <div className='index-filter-container' ref={priceList} onBlur={e => handleBlur(e, setPriceListVisible)} tabIndex="-1" onClick={e => e.stopPropagation()}>
-                            <form action="" onSubmit={ev => onfilterByPriceSubmit(ev)}>
-
-                                <div className='opt' >
-                                    <input type="radio" id="huey" name="price" value="50" defaultChecked />
-                                    <label htmlFor="huey" ><span >value</span>Under $50</label>
-                                </div>
-
-                                <div className='opt'>
-                                    <input type="radio" id="dewey" name="price" value="100" />
-                                    <label htmlFor="dewey" ><span >value</span>Under $100</label>
-                                </div>
-
-                                <div className='opt'>
-                                    <input type="radio" id="louie" name="price" value="200" />
-                                    <label htmlFor="louie"><span>value</span>Under $200</label>
-                                </div>
-
-                                <div className='costum opt'>
-                                    <input type="radio" id="louie" name="price" value={costumPrice} />
-                                    <label htmlFor="louie">Costum
-                                        <br />
-                                        <input type="number" name="" id="" onChange={e => onChamgeCostumPrice(e.target.value)} placeholder='Enter Budget                     $' />
-                                    </label>
-                                </div>
-
-                                <div className='btn-container'>
-                                    <button type="submit" className='apply'>Apply</button>
-                                </div>
-                            </form>
-                        </div>}
-                </div>
-
-                {/* dates */}
-                <div className="filter-btn" onClick={() => setDateListVisible(prev => !prev)} tabIndex="-1" data-preventchildblur="date-btn"><span>Delivery time</span>
-                    {dateOptionsVisible &&
-                        <div className='index-filter-container' ref={dateList} onBlur={e => handleBlur(e, setDateListVisible)} tabIndex="-1" onClick={e => e.stopPropagation()}>
-                            <form action="" onSubmit={ev => onfilterByDateSubmit(ev)}>
-                                <div className='opt'>
-                                    <input type="radio" id="huey" name="date" value="1" />
-                                    <label htmlFor="huey"><span>Express 24H</span></label>
-                                </div>
-
-                                <div className='opt'>
-                                    <input type="radio" id="dewey" name="date" value="3" />
-                                    <label htmlFor="dewey"><span>Up to 3 days</span></label>
-                                </div>
-
-                                <div className='opt'>
-                                    <input type="radio" id="louie" name="date" value="7" />
-                                    <label htmlFor="louie"><span>Up to 7 days</span></label>
-                                </div>
-
-                                <div className='opt'>
-                                    <input type="radio" id="fiji" name="date" value="0" defaultChecked />
-                                    <label htmlFor="fiji"><span>Anytime</span></label>
-                                </div>
-
-                                <div className='btn-container'>
-                                    <button type="submit" className='apply'>Apply</button>
-                                </div>
-                            </form>
-                        </div>}
-                </div>
-            </div>
-
             {/* <FilterBtn /> */}
             <GigFilter filterBy={filterBy} setFilterBy={onSetFilterBy} />
             <GigList
